67a3261aaf252069d188cbd2ea3fccb954e29784
bishal <bhattabishal330@gmail.com>
2023-05-24 17:34:15 +0545

    update3

---
+++ first.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+  <div class="container">
+    <div class="mb-3">
+      <label for="" class="form-label">Please enter a data to sent to next page</label>
+      <input type="text"
+        class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+    </div>
+  </div>
+</body>
+
+</html>
